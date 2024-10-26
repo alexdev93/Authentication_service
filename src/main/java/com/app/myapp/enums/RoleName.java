066983028e7113675d7bde0174938ed4ca917af4
alexdev <alemayehu@addissoftware.com>
2024-10-26 21:56:09 +0300

    improve

--- src/main/java/com/app/myapp/enums/RoleName.java
+++ src/main/java/com/app/myapp/enums/RoleName.java
@@ -4,21 +4,4 @@ public enum RoleName {
     SUPER_ADMIN,
     ADMIN,
     USER;
-
-    public String getRole() {
-        return "ROLE_" + name();
-    }
-
-    public static String[] getAllRoles() {
-        return new String[] { SUPER_ADMIN.getRole(), ADMIN.getRole(), USER.getRole() };
-    }
-
-    public static String[] getAdminAndUserRoles() {
-        return new String[] { ADMIN.getRole(), USER.getRole(), SUPER_ADMIN.getRole() };
-    }
-
-    @Override
-    public String toString() {
-        return getRole();
-    }
 }
