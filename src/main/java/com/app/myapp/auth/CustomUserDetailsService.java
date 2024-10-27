350bb3624accc26712d3feb3674494eaf84b537d
alexdev <alemayehu@addissoftware.com>
2024-10-27 08:08:58 +0300

    improve role based access using user authorities

--- src/main/java/com/app/myapp/auth/CustomUserDetailsService.java
+++ src/main/java/com/app/myapp/auth/CustomUserDetailsService.java
@@ -1,15 +1,19 @@
 package com.app.myapp.auth;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.app.myapp.role.Role;
 import com.app.myapp.user.User;
 import com.app.myapp.user.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,17 +27,22 @@ public class CustomUserDetailsService implements UserDetailsService {
         if (user == null) {
             throw new UsernameNotFoundException("User not found: " + username);
         }
-        String[] roles = user.getRoles() == null
-                ? new String[0]
+
+        // Convert roles to GrantedAuthority
+        List<GrantedAuthority> authorities = user.getRoles() == null
+                ? List.of() // or Collections.emptyList()
                 : user.getRoles().stream()
-                        .map(Role::getName)
-                        .toArray(String[]::new);
+                        .map(role -> new SimpleGrantedAuthority(role.getName().name())) // Assuming RoleName is an enum
+                        .collect(Collectors.toList());
 
         return org.springframework.security.core.userdetails.User.withUsername(user.getUsername())
                 .password(user.getPassword())
-                .roles(roles)
+                // .roles(authorities)
+                .authorities(authorities) // Set authorities instead of roles
+                .accountExpired(false) // Set as needed
+                .accountLocked(false) // Set as needed
+                .credentialsExpired(false) // Set as needed
+                .disabled(false) // Set as needed
                 .build();
-
     }
-
 }
