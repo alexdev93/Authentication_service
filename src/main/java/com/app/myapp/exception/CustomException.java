c9f23832fd3b88cc6f9d5c9f4298f6045e63442a
alexdev <alemayehu@addissoftware.com>
2024-10-27 09:25:40 +0300

    improve

--- src/main/java/com/app/myapp/exception/CustomException.java
+++ src/main/java/com/app/myapp/exception/CustomException.java
@@ -10,37 +10,31 @@ public class CustomException extends RuntimeException {
 
     private final HttpStatus status;
 
-    // Default Constructor
     public CustomException() {
         super();
-        this.status = HttpStatus.INTERNAL_SERVER_ERROR; // Default to 500 Internal Server Error
+        this.status = HttpStatus.INTERNAL_SERVER_ERROR;
     }
 
-    // Constructor with message
     public CustomException(String message) {
         super(message);
-        this.status = HttpStatus.INTERNAL_SERVER_ERROR; // Default to 500
+        this.status = HttpStatus.INTERNAL_SERVER_ERROR;
     }
 
-    // Constructor with message and cause
     public CustomException(String message, Throwable cause) {
         super(message, cause);
         this.status = HttpStatus.INTERNAL_SERVER_ERROR;
     }
 
-    // Constructor with cause
     public CustomException(Throwable cause) {
         super(cause);
         this.status = HttpStatus.INTERNAL_SERVER_ERROR;
     }
 
-    // Constructor with custom status and message
     public CustomException(HttpStatus status, String message) {
         super(message);
         this.status = status;
     }
 
-    // Constructor with custom status, message, and cause
     public CustomException(HttpStatus status, String message, Throwable cause) {
         super(message, cause);
         this.status = status;
--- src/main/java/com/app/myapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/myapp/exception/GlobalExceptionHandler.java
@@ -17,14 +17,12 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 @RestController
 public class GlobalExceptionHandler {
 
-    // Generic Exception Handler
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex, WebRequest request) {
         ErrorResponse errorResponse = createErrorResponse(ex.getMessage(), request, HttpStatus.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    // Custom Exception Handler
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex, WebRequest request) {
         ErrorResponse errorResponse = createErrorResponse(ex.getMessage(), request, HttpStatus.BAD_REQUEST);
@@ -52,31 +50,26 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(errors);
     }
 
-    // Handle UsernameAlreadyExistsException
     @ExceptionHandler(UsernameAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException ex,
             WebRequest request) {
         ErrorResponse errorResponse = createErrorResponse(
                 "Username already exists",
                 request,
-                HttpStatus.CONFLICT // 409 Conflict
-        );
+                HttpStatus.CONFLICT);
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
-    // Handle Invalid Credntial
     @ExceptionHandler(InvalidCredntial.class)
     public ResponseEntity<ErrorResponse> handleUsernameAlreadyExistsException(InvalidCredntial ex,
             WebRequest request) {
         ErrorResponse errorResponse = createErrorResponse(
                 "Invalid credintial",
                 request,
-                HttpStatus.FORBIDDEN // 403 forbidden
-        );
+                HttpStatus.FORBIDDEN);
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
-    // Handle Not Found Exception
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(
             NotFoundException ex,
@@ -84,12 +77,10 @@ public class GlobalExceptionHandler {
         ErrorResponse errorResponse = createErrorResponse(
                 ex.getMessage(),
                 request,
-                HttpStatus.NOT_FOUND // 404 forbidden
-        );
+                HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
-    // Method to create a standardized error response
     private ErrorResponse createErrorResponse(String message, WebRequest request, HttpStatus status) {
         return new ErrorResponse(
                 message,
