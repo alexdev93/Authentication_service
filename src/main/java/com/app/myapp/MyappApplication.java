637434d54f85544817172f0d230cd34f36c57cf5
alexdev <alemayehu@addissoftware.com>
2024-10-28 08:22:17 +0300

    add mail provider

--- src/main/java/com/app/myapp/MyappApplication.java
+++ src/main/java/com/app/myapp/MyappApplication.java
@@ -2,8 +2,10 @@ package com.app.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MyappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/app/myapp/utils/Helper.java
+++ src/main/java/com/app/myapp/utils/Helper.java
@@ -2,6 +2,9 @@ package com.app.myapp.utils;
 
 import java.io.IOException;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,9 +16,21 @@ import lombok.RequiredArgsConstructor;
 public class Helper {
 
     private final ResponseHandler responseHandler;
+    private final JavaMailSender mailSender;
 
     public void handleServletResponse(HttpServletRequest request, HttpServletResponse response,
             int statusCode, String error, String message) throws IOException {
         responseHandler.writeErrorResponse(request, response, statusCode, error, message);
+    }
+
+    @Async
+    public void sendVerificationEmail(String toEmail, String verificationUrl) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("Email Verification");
+        message.setText("Click the following link to verify your email: " + verificationUrl);
+
+        // Send email asynchronously
+        mailSender.send(message);
     }
 }
